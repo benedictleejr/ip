5e4ce9e9f621cfb3952db2f2c06762ae277b90af
benedictleejr <belasolee@gmail.com>
2025-01-28 01:45:17 +0800

    Add save functionality and persistent memory to tasklist

--- src/main/java/Tasklist.java
+++ src/main/java/Tasklist.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -6,6 +13,19 @@ public class Tasklist {
 
     public Tasklist() {
         this.tasklist = new ArrayList<>();
+    }
+
+    public Tasklist(ArrayList<Task> tasklist) {
+        this.tasklist = tasklist;
+    }
+
+    /**
+     * Returns the tasklist.
+     * 
+     * @return the tasklist
+     */
+    public ArrayList<Task> getTasklist() {
+        return this.tasklist;
     }
 
     /**
@@ -78,6 +98,48 @@ public class Tasklist {
 
                 """, t.getTaskType(), t.getStatusIcon(), t.getDescription(), tasklist.size());
         pw.println(output);
+    }
+
+    /**
+     * Saves all current tasks to a file, for persistent memory.
+     * 
+     * @param filePath
+     */
+    public void saveTasks(String filePath) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(this.tasklist);
+            System.out.println("Tasks saved to file."); // REMOVE for final version
+        } catch (IOException e) {
+            System.err.println("Error saving tasks to file: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Loads tasks from a file.
+     * 
+     * @param filePath path to the file to load tasks from
+     * @return a Tasklist object containing the tasks loaded from the file
+     */
+    @SuppressWarnings("unchecked")
+    public static Tasklist loadTasks(String filePath) {
+        ArrayList<Task> tasklist = new ArrayList<>();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            tasklist = (ArrayList<Task>) ois.readObject();
+
+        } catch (FileNotFoundException e) { // if file not found, then create the new file
+            File file = new File(filePath);
+            try {
+                file.createNewFile();
+            } catch (IOException e1) {
+                System.err.println("Error creating new file: " + e1.getMessage());
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error reading tasks from file: " + e.getMessage());
+        }
+        return tasklist.isEmpty() ? new Tasklist() : new Tasklist(tasklist);
     }
 
 }
