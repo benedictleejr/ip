9f51135e71c0d3fd164d1d3caa2ad9e29b80f919
benedictleejr <belasolee@gmail.com>
2025-02-06 11:28:31 +0800

    Add JavaFX functionality - WIP

---
+++ src/main/java/wooper/MainWindow.java
@@ -0,0 +1,54 @@
+package wooper;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Wooper wooper;
+
+    // TODO: Change user image
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/wooper.png"));
+    private Image wooperImage = new Image(this.getClass().getResourceAsStream("/images/wooper.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Wooper instance */
+    public void setWooper(Wooper w) {
+        wooper = w;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing
+     * Wooper's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = wooper.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getWooperDialog(response, wooperImage));
+        userInput.clear();
+    }
+}
--- src/main/java/wooper/Wooper.java
+++ src/main/java/wooper/Wooper.java
@@ -26,6 +26,13 @@ public class Wooper {
     }
 
     /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+    /**
      * Main method to run the Wooper chatbot.
      * @throws IOException
      */
